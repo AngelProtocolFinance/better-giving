2e7e6068dab64249c784d3feee2581a43015515c
ap-justin <89639563+ap-justin@users.noreply.github.com>
2026-06-20 00:57:17 +0800

    refactor: snake_case id_param_to_num

--- src/helpers/__tests__/id-param-to-num.test.ts
+++ src/helpers/__tests__/id-param-to-num.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, test } from "vitest";
-import { idParamToNum } from "../id-param-to-num";
+import { id_param_to_num } from "../id-param-to-num";
 
 describe("id param to number", () => {
   test("user set id path to invalid", () => {
     //user types to url invalid
-    expect(idParamToNum()).toBe(0); //0 is AP_ID
-    expect(idParamToNum("abcde")).toBe(0);
-    expect(idParamToNum("1.123")).toBe(1);
-    expect(idParamToNum("2.6123")).toBe(2);
+    expect(id_param_to_num()).toBe(0); //0 is AP_ID
+    expect(id_param_to_num("abcde")).toBe(0);
+    expect(id_param_to_num("1.123")).toBe(1);
+    expect(id_param_to_num("2.6123")).toBe(2);
   });
   test("valid string numbers", () => {
-    expect(idParamToNum("10")).toBe(10);
-    expect(idParamToNum("100")).toBe(100);
+    expect(id_param_to_num("10")).toBe(10);
+    expect(id_param_to_num("100")).toBe(100);
   });
 });
